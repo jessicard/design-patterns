0386e45f06188479407e9075f02c9e30492788c8
Jessica Card <jessicard@mac.com>
2022-12-29 17:20:39 -0500

    Add new section

---
+++ observer/WeatherMonitor/src/WeatherData.java
@@ -0,0 +1,23 @@
+public class WeatherData {
+    private float temperature;
+    private float humidity;
+    private float pressure;
+    public float getTemperature() {
+        return temperature;
+    }
+    public float getHumidity() {
+        return humidity;
+    }
+    public float getPressure() {
+        return pressure;
+    }
+    public void measurementsChanged() {
+        float temp = getTemperature();
+        float humidity = getHumidity();
+        float pressure = getPressure();
+
+        currentConditionsDisplay.update(temp, humidity, pressure);
+        statisticsDisplay.update(temp, humidity, pressure);
+        forecastDisplay.update(temp, humidity, pressure);
+    }
+}
---
+++ observer/WeatherMonitor/src/WeatherMonitor.java
@@ -0,0 +1,5 @@
+public class WeatherMonitor {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
