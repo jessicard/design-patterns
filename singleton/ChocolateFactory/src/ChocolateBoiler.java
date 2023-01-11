d50fe9eeb0c83f32d5a513983fa975981fd4eb27
Jessica Card <jessicard@mac.com>
2023-01-10 20:30:57 -0500

    Chocolate factory for singletons

---
+++ singleton/ChocolateFactory/src/ChocolateBoiler.java
@@ -0,0 +1,33 @@
+public class ChocolateBoiler {
+    private boolean empty;
+    private boolean boiled;
+    public ChocolateBoiler() {
+        empty = true;
+        boiled = false;
+    }
+    public void fill() {
+        if (isEmpty()) {
+            empty = false;
+            boiled = false;
+            // fill the boiler with a milk/chocolate mixture
+        }
+    }
+    public void drain() {
+        if (!isEmpty() && isBoiled()) {
+            // drain the boiled milk and chocolate
+            empty = true;
+        }
+    }
+    public void boil() {
+        if (!isEmpty() && !isBoiled()) {
+            // bring the contents to a boil
+            boiled = true;
+        }
+    }
+    public boolean isEmpty() {
+        return empty;
+    }
+    public boolean isBoiled() {
+        return boiled;
+    }
+}
